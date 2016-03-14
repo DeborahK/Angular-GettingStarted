73415c4352c8011e69a7409c8b3806d7829c312b
DeborahK <deborahk@insteptech.com>
2016-03-14 10:12:56 -0700

    Minor formatting

--- APM - Start/app/main.ts
+++ APM - Start/app/main.ts
@@ -1,6 +1,6 @@
-import {bootstrap}    from 'angular2/platform/browser';
+import { bootstrap } from 'angular2/platform/browser';
 
 // Our main component
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
