ed15973415082a97f5bf6afea9e4de4d0d1e31f0
DeborahK <deborahk@insteptech.com>
2016-03-15 18:48:37 -0700

    Minor formatting changes

--- APM - Final/app/products/product-filter.pipe.ts
+++ APM - Final/app/products/product-filter.pipe.ts
@@ -1,5 +1,6 @@
-import {Pipe, PipeTransform} from 'angular2/core';
+import { Pipe, PipeTransform } from 'angular2/core';
 import { IProduct } from './product';
+
 @Pipe({
     name: 'productFilter'
 })
