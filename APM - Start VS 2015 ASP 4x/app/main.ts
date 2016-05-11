67c0c4c741b2d3afe8f96519252eed008f03d025
Deborah Kurata <deborahk@insteptech.com>
2016-05-11 15:36:20 -0700

    Starter files for VS 2015 using ASP.NET 4.x

---
+++ APM - Start VS 2015 ASP 4x/app/main.ts
@@ -0,0 +1,7 @@
+///<reference path="../typings/browser.d.ts"/>
+import { bootstrap } from 'angular2/platform/browser';
+
+// Our main component
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
