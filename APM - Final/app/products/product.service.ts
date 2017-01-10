96e3f5e20c51f49eec885544457b00c82cc357f1
Deborah Kurata <deborahk@insteptech.com>
2017-01-10 10:18:07 -0800

    Add import for Observable.throw

--- APM - Final/app/products/product.service.ts
+++ APM - Final/app/products/product.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
 
 import { IProduct } from './product';
 
