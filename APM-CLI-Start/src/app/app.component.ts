851d53dfa3d1a2d26d16d2457e69954dc55cd4f4
Deborah Kurata <deborahk@insteptech.com>
2017-06-21 11:13:25 -0700

    Add APM-CLI-Start

---
+++ APM-CLI-Start/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
