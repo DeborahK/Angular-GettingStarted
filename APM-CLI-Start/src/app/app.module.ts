22c426b6a9719ae961a38c39465ae53d4ae88f99
Deborah Kurata <deborahk@insteptech.com>
2017-06-23 01:10:54 -0700

    Complete first cut of starter files.

--- APM-CLI-Start/src/app/app.module.ts
+++ APM-CLI-Start/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from 'app/home/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
