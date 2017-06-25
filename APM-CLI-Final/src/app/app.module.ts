12dd8b914123d0c77e8bd71caebcbb0b14f91b20
Deborah Kurata <deborahk@insteptech.com>
2017-06-25 15:29:06 -0700

    Remove WelcomeComponent from start

--- APM-CLI-Final/src/app/app.module.ts
+++ APM-CLI-Final/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from 'app/home/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
