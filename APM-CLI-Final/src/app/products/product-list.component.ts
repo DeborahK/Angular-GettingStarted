c0f4aa3a0ede13bf45343b855bcc9fab17b60caf
Deborah Kurata <deborahk@insteptech.com>
2017-07-01 01:50:58 -0700

    As of Module 7

--- APM-CLI-Final/src/app/products/product-list.component.ts
+++ APM-CLI-Final/src/app/products/product-list.component.ts
@@ -50,6 +50,10 @@ export class ProductListComponent implements OnInit {
         this.listFilter = 'cart';
     }
 
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
--- APM-CLI-Final/src/app/products/product.ts
+++ APM-CLI-Final/src/app/products/product.ts
@@ -8,3 +8,4 @@ export interface IProduct {
     starRating: number;
     imageUrl: string;
 }
+
