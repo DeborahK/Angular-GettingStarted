76caf1a050a29534baacb035fff20966e3b9bf0a
Deborah Kurata <deborahk@insteptech.com>
2017-07-01 14:56:41 -0700

    As of Module 8

--- APM-CLI-Final/src/app/app.component.ts
+++ APM-CLI-Final/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
   selector: 'pm-root',
   template: `
     <div><h1>{{pageTitle}}</h1>
         <pm-products></pm-products>
     </div>
-    `
+    `,
+    providers: [ ProductService ]
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
--- APM-CLI-Final/src/app/products/product-list.component.ts
+++ APM-CLI-Final/src/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -22,32 +24,10 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _productService: ProductService) {
+
     }
 
     onRatingClicked(message: string): void {
@@ -65,6 +45,7 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
