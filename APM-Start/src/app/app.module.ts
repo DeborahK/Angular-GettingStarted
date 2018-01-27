2d54ff3ae672bf9f4d07d9d3297a183cd57d1e09
Taylor Etheredg <taylor.etheredge@gmail.com>
2018-01-27 14:05:17 -0600

    add spacing between brackets and AppComponent in app.module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -11,6 +11,6 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
