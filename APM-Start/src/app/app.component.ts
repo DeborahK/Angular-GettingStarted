efb32a0e7005e371ecc59c01c09bf471b23bc838
Taylor Etheredg <taylor.etheredge@gmail.com>
2018-01-27 14:04:26 -0600

    adjust property name and string for the AppComponent class

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
