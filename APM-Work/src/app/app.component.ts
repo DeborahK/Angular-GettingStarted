775900a2184ee214a1ae09ed2ffaa7e377894241
Taylor Etheredg <taylor.etheredge@gmail.com>
2018-01-27 14:15:02 -0600

    adding working directory to make changes in

---
+++ APM-Work/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
