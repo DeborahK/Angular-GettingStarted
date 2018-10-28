dc2950f4cb5c6aaf14c40b7608212f5a32c585fe
Mallikarjuna Kalkere <mkalkere@gmail.com>
2018-10-28 00:44:49 -0400

    Templates,Interpolation, and Directives

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   template: `
               <div>
                 <h1>{{pageTitle}}</h1>
-                <div>My First Component</div>
+                <pm-products></pm-products>
               </div>
             ` 
 })
