a97d996b4d9c8afdf9c8183b26790407bfd6523e
Mallikarjuna Kalkere <mkalkere@gmail.com>
2018-10-28 13:49:26 -0400

    Data Binding & Pipes

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent{
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 1,
@@ -28,4 +32,8 @@ export class ProductListComponent{
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
           }
     ];
+
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
 }
