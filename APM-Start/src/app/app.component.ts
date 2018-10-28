43256c894648aab7598af1a9d51621bad35ebaed
Mallikarjuna Kalkere <mkalkere@gmail.com>
2018-10-27 22:43:32 -0400

    Introduction to Templates

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+              <div>
+                <h1>{{pageTitle}}</h1>
+                <div>My First Component</div>
+              </div>
+            ` 
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle: string = 'Acme Product Management';
 }
