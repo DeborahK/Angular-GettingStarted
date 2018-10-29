72c5fc82b1f27ebe6ad018f6fa6d042dc202eb38
Mallikarjuna Kalkere <mkalkere@gmail.com>
2018-10-28 20:09:07 -0400

    More on Components

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
---
+++ APM-Start/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+export class ConvertToSpacesPipe implements PipeTransform {
+    transform(value: string, character: string) : string {
+        return value.replace(character, ' ');
+    }
+}
