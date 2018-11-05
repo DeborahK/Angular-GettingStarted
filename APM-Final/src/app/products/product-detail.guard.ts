66e4cf764b697b018e03a664b5c980c9f795432c
Deborah Kurata <deborahk@insteptech.com>
2018-11-05 11:59:42 -0800

    Add altternate syntax to read the URL from the route.

--- APM-Final/src/app/products/product-detail.guard.ts
+++ APM-Final/src/app/products/product-detail.guard.ts
@@ -13,6 +13,8 @@ export class ProductDetailGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const id = +next.url[1].path;
+    // const id2 = next.paramMap.get('id');
+    // console.log(id2);
     if (isNaN(id) || id < 1) {
       alert('Invalid product Id');
       this.router.navigate(['/products']);
