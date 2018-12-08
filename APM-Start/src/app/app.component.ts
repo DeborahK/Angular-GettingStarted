e40077cc98026850d89df84cce3d544344f2af29
Nivritii Ramachandran <n.d.ramachandran@avanade.com>
2018-12-08 20:34:13 +0800

    Revert "apm-start-begins"
    This reverts commit 0c6f23ad0d28513251eac3c75dd09818414c2d34.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-    </div>
-    `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Project Management';
+  title = 'Angular: Getting Started';
 }
