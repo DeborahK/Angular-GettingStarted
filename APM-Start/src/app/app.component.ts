7bb50cea836c2970c9e5f2f3f109c4e25203fbcb
Stephen <stephendeanharper@gmail.com>
2019-01-30 11:54:49 -0800

    created component with typescript in angular

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  <div>
+  `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
