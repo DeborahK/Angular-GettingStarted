8492a4c14eafcbfe63381d8530d0ef98a3b41d52
Stephen <stephendeanharper@gmail.com>
2019-01-31 21:43:47 -0800

    added event handling for showing and hiding images. added in images for each item displayed and attached properties to resize the images

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] =
   [
     {
@@ -40,4 +43,7 @@ export class ProductListComponent {
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }
   ];
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
