5ded61f4903f9e96bc7d67a5ae863fe72d2a82be
Juan Vargas <jvargasc@gmail.com>
2019-04-20 19:06:20 -0400

    Introduction To Components.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+      </div>
+    `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Acme PRoduct Management';
 }
