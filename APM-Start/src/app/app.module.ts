2ba19c7e661cfa013f1f4ede68060ab5e5ced5c2
Juan Vargas <jvargasc@gmail.com>
2019-05-05 13:15:27 -0400

    Navigation and Routing Additional Techniques

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,9 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/: id ', component : ProductDetailComponent },
+      { path: 'products/:id', 
+        canActivate: [ ProductDetailGuard ],
+        component : ProductDetailComponent },
       { path: 'welcome', component : WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
+
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +11,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
   
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/4/41/Wooden_rake.jpg"
+    }
   }
 
+  onBack(): void{
+    this.router.navigate(['/products']);
+  }
 }
