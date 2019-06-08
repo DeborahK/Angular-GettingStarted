66b9163cce642b9a8de92ec4b7a6648eb067ba15
Christopher Rudy <crudysandiego@Christophers-MacBook-Pro-2.local>
2019-06-08 12:49:15 -0700

    implementation

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product';
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
+})
+
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = 'carts';
+  products: IProduct[] = [
+    {
+        'productId': 1,
+        'productName': 'Leaf Rake',
+        'productCode': 'GDN-0011',
+        'releaseDate': 'March 19, 2016',
+        'description': 'Leaf rake with 48-inch wooden handle.',
+        'price': 19.95,
+        'starRating': 3.2,
+        'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    },
+    {
+        'productId': 2,
+        'productName': 'Garden Cart',
+        'productCode': 'GDN-0023',
+        'releaseDate': 'March 18, 2016',
+        'description': '15 gallon capacity rolling garden cart',
+        'price': 32.99,
+        'starRating': 4.2,
+        'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    }
+  ];
+
+  toggleImage():void {
+    this.showImage = !this.showImage;
+  }
+}
