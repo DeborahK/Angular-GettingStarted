175e373d472c621b7a76ee960533fe644e0e49d4
Juan Vargas <jvargasc@gmail.com>
2019-07-01 11:19:40 -0400

    Issue with the API service solved.

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,42 +1,30 @@
-import { Injectable } from "@angular/core";
-import { IProduct } from "./product";
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
-import { version } from "punycode";
+import { version } from 'punycode';
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class ProductService{
-      //private productUrl = 'api/products/products.json';
-      //private productUrl = 'assets/products/products.json';
-      private productUrl = 'https://localhost:6001/api';
+export class ProductService {
+      // private productUrl = 'api/products/products.json';
+      // private productUrl = 'assets/products/products.json';
+      // private productUrl = 'https://localhost:6001/api';
+      private productUrl = 'https://pipelinesangularapijv.azurewebsites.net/api';
 
     constructor(private http: HttpClient) {
 
     }
 
    getProducts(): Observable<IProduct[]> {
-        // return this.http.get<IProduct[]>(this.productUrl + '/products' ).pipe(
-        //     tap(data => console.log('All: ' + JSON.stringify(data))),
-        //     catchError(this.handleError)
-        // );
         return this.http.get<IProduct[]>(`${this.productUrl}/products`);
     }
 
-    // getProducts(): Observable<IProduct[]> {
-    //     return this.http.get<IProduct[]>(this.productUrl).pipe(
-    //         tap(data => console.log('All: ' + JSON.stringify(data))),
-    //         catchError(this.handleError)
-    //     );
-    // }
-
     getProduct(id: number): Observable<IProduct | undefined> {
-        return this.getProducts().pipe(
-            map((products: IProduct[]) => products.find(p => p.productId === id))
-        );
+        return this.http.get<IProduct>(`${this.productUrl}/products/${id}`);
     }
 
     private handleError(err: HttpErrorResponse) {
--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -27,6 +27,11 @@ namespace Products.API
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors((p)=> {
+				p.AddDefaultPolicy((x)=> {
+					x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+				});
+			});
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 		}
 
@@ -42,7 +47,7 @@ namespace Products.API
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-
+			app.UseCors();
 			app.UseHttpsRedirection();
 			app.UseMvc();
 		}
