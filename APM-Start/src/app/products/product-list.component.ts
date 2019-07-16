bcb62755a78a46ccc9b17b1f408254f5628f068d
GerardoHP <verdaderoperrodelmal@hotmail.com>
2019-07-16 18:28:46 -0500

    Using a component as a directive

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+}
