428894c2444d022a9f831a806ca20b215a0baa88
adotaibi <adotaibi@THQ1LTADOTAIBI>
2019-10-28 13:07:35 +0300

    service injection

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -22,28 +23,9 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[] = [];
-    products: IProduct[] = [{
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2019",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      }];
-      constructor() {
-        this.filteredProducts = this.products;
+    products: IProduct[] = [];
+      constructor( private productService : ProductService) {
+        
       }
       performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
@@ -54,9 +36,10 @@ export class ProductListComponent implements OnInit{
         this.showImage = !this.showImage;
       }
       ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
       }
       onRatingClicked(message : string) : void {
-        this.pageTitle = 'Product List: ' + message;
+        this.pageTitle = 'Product List: ' + message;        
       }
 }
