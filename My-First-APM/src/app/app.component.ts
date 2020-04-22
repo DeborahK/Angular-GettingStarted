660ee3a04b65c7d7e100f8e462ab5891a0d22215
bodhibrdr <julie.gumerman@gmail.com>
2020-04-22 16:01:34 -0600

    making an app...one step at a time

--- My-First-APM/src/app/app.component.ts
+++ My-First-APM/src/app/app.component.ts
@@ -1,10 +1,26 @@
+// import { Component } from '@angular/core';
+
 import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   totalWin = 'This Angular Life';
+//   julieWasHere = 'I do work';
+// }
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{ pageTitle }}</h1>
+    <div>My First Component</div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "Acme Product Management"
 }
