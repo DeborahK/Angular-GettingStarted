78ceeb41af0a3ba55ad3d3cb23fae0dcb5bff6fd
bodhibrdr <julie.gumerman@gmail.com>
2020-04-28 16:02:56 -0600

    we have some functionality! finally!

--- My-First-APM/src/app/app.component.ts
+++ My-First-APM/src/app/app.component.ts
@@ -3,25 +3,34 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from "./products/product-list.component";
 
-// @Component({
-//   selector: 'pm-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   totalWin = 'This Angular Life';
-//   julieWasHere = 'I do work';
-// }
+
 
 @Component({
   selector: 'pm-root',
   template: `
-  <div>
-    <h1>{{ pageTitle }}</h1>
-    <pm-products></pm-products>
-  </div>
-  `
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='nav nav-pills'>
+        <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+        <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+      </ul>
+    </nav>
+    <div class='container'>
+    <router-outlet></router-outlet>
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   pageTitle: string = "Acme Product Management"
 }
+
+// <nav class='navbar navbar-expand navbar-light bg-light'>
+// <ul class='nav nav-pills'>
+//     <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+//     <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+// </ul>
+// </nav>
+// <div class='container'>
+// <router-outlet></router-outlet>
+// </div>
