0a8d5ede20943c3867d4648781c8550789c804fd
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-06-27 21:52:43 -0700

    Building, registering and injecting svc

--- APM-Start/src/app/products/products-list.component.ts
+++ APM-Start/src/app/products/products-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './IProduct';
+import { ProductService } from './products.service';
 
 @Component({
   selector: 'pm-products',
@@ -13,7 +14,9 @@ export class ProductsListComponent implements OnInit {
   }
   public set listFilter(v: string) {
     this._listFilter = v;
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
   }
 
   pageTitle = 'Products list';
@@ -21,51 +24,29 @@ export class ProductsListComponent implements OnInit {
   imageMargin = 2;
   showImages = true;
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-  ];
-  /**
-   *
-   */
-  constructor() {
-    this.filteredProducts = this.products;
-    this._listFilter = '';
+  products: IProduct[];
+
+  constructor(private productService: ProductService) {
   }
 
   toggleImages() {
     this.showImages = !this.showImages;
   }
 
-
   onRatingClicked(message: string): void {
     this.pageTitle = `Product list: ${message}`;
   }
-  
+
   performFilter(filterBy: string): IProduct[] {
     const filter = filterBy.toLocaleLowerCase();
-    return this.products.filter(e => e.productName.toLocaleLowerCase().indexOf(filter) !== -1);
+    return this.products.filter(
+      (e) => e.productName.toLocaleLowerCase().indexOf(filter) !== -1
+    );
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+    this._listFilter = '';
   }
 }
