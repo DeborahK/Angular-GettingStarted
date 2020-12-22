617e61839ec32d742091704544cffea392e24837
Juan III De los santos <j3delossantos@outlook.com>
2020-12-21 20:20:03 -0400

    initial comit first componet

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import{ Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Angular: Getting Started';
+ selector: 'pm-root',
+ template:`
+    <div><h1>{{pageTitle}}</h1>
+        <div>My first Component</div>
+    </div>
+    `
+  })
+export class AppComponent{
+  pageTitle: string = 'Acme Product Management'; 
 }
+
