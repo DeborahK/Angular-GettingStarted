f1777dba883eedeeee7db53090569118833be0b7
Juan III De los santos <j3delossantos@outlook.com>
2020-12-21 22:22:43 -0400

    Usin structural directive to display records, complited chapter 5

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,7 +4,7 @@ import{ Component } from '@angular/core';
  selector: 'pm-root',
  template:`
     <div><h1>{{pageTitle}}</h1>
-        <div>My first Component</div>
+        <pm-products></pm-products>
     </div>
     `
   })
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {ProductListComponent} from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
