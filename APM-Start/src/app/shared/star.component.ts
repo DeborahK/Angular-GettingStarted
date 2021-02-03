74b30333fa258a6b84c3e5701108baf4a10e7f27
Naasgul <lnaas@hotmail.de>
2021-02-03 14:01:55 +0100

    ✨ Add onClick method to display star rating on top

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 @Component({
     selector:"pm-star",
@@ -7,10 +7,15 @@ import { Component, OnChanges } from "@angular/core";
 })
 
 export class StarComponent implements OnChanges {
-rating:number =4;
+@Input() rating:number;
 starWidth: number;
+@Output() ratingClicked:EventEmitter<string> = new EventEmitter<string>();
 
 ngOnChanges():void {
     this.starWidth=this.rating*75/5;
+}
+onClick():void {
+    //Warum klappt this.rating hier?
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`)
 }
 }
