fc7fe1e5cc0ed120b8056c42b8cb0006ac7ebfc9
Naasgul <lnaas@hotmail.de>
2021-02-03 11:42:17 +0100

    ✨ Add filter method and implement in template

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,16 +1,34 @@
-import {Component } from "@angular/core"
+import {Component, OnInit } from "@angular/core"
+import { IProduct } from "./product";
+
 
 @Component({
     selector:"pm-products",
-    templateUrl:"./product-list.component.html"
+    templateUrl:"./product-list.component.html",
+    styleUrls:["./product-list.component.css"]
 })
-export class ProductListComponent {
+export class ProductListComponent 
+implements OnInit
+{
 heading:string ="Product List";
 imageWidth:number= 50;
 imageMargin:number=2;
 showImage:boolean=false;
-listFilter:string="cart";
-products:any[] = [ {
+
+
+_listFilter:string="cart";
+
+get listFilter():string{
+  return this._listFilter
+}
+set listFilter(value:string){
+  this._listFilter = value;
+  this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products
+}
+filteredProducts: IProduct[];
+
+
+products:IProduct[] = [ {
     "productId": 1,
     "productName": "Leaf Rake",
     "productCode": "GDN-0011",
@@ -30,6 +48,20 @@ products:any[] = [ {
     "starRating": 4.2,
     "imageUrl": "assets/images/garden_cart.png"
   }];
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter ="cart"
+  }
+
+  performFilter(filterBy:string):IProduct[] {
+    filterBy=filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct)=> product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1)
+  }
+
+  ngOnInit(): void {
+    console.log("Hallo OnInit")
+  }
 
   toggleImage():void{
     this.showImage=!this.showImage;
