d8823fc7c7e602b59821afdf20f809b08ebbd118
Naasgul <lnaas@hotmail.de>
2021-02-03 08:11:56 +0100

    ✨ Add product list component

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,29 @@
+import {Component } from "@angular/core"
+
+@Component({
+    selector:"pm-products",
+    templateUrl:"./product-list.component.html"
+})
+export class ProductListComponent {
+heading:string ="Product List";
+products:any[] = [ {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2019",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+  },
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2019",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+  }];
+}
