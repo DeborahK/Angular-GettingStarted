de03d90a2b392e902904f1e0e6fe09a585a3d962
Naasgul <lnaas@hotmail.de>
2021-02-03 08:11:24 +0100

    ⚡️ Modify App Component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `<div> 
+  <h2> Hallo </h2>
+  <pm-products> </pm-products>
+  </div>`
+  
+  
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
