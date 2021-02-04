016fa1ac8af1baaacb4449522683ea95c1c9f561
Naasgul <lnaas@hotmail.de>
2021-02-04 13:00:25 +0100

    🚚  Add basic routing

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,15 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div> 
-  <h2> Hallo </h2>
-  <pm-products> </pm-products>
-  </div>`
+
+  
+  template: `
+  <nav vlass="navbar navbar-expand navbar-light bg-light">
+
+<a class="navbar-brand"> {{pageTitle}} </a>
+<ul class="nav nav-pills">
+<li><a class="nav-link" [routerLink]="['/welcome']"> Home </a></li>
+<li><a class="nav-link" [routerLink]="['/products']"> Product List </a>
+
+
+</ul>
+
+
+
+</nav>
+<div class="container">
+<router-outlet></router-outlet>
+</div>
+  `
   
   
   ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Angular: Getting Started';
 }
---
+++ APM-Start/src/app/products/product-detail.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailGuard implements CanActivate {
+
+constructor(private router: Router) {
+
+}
+
+canActivate(
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  const id = +next.url[1].path;
+  // const id2 = next.paramMap.get('id');
+  // console.log(id2);
+  if (isNaN(id) || id < 1) {
+    alert('Invalid product Id');
+    this.router.navigate(['/products']);
+    return false;
+  }
+  return true;
+}
+
+
+
+  
+}
