6fc92f289cf729835ca062aa9fe13135cf0889e7
Carlos Rossi <carlos.rossi@ukg.com>
2022-05-27 13:47:48 -0400

    so,;old,fsdf

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -16,7 +17,8 @@ import { StarComponent } from './shared/star.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/product-list/product-list.component.ts
+++ APM-Start/src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Product } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     'selector':'product-list',
@@ -8,12 +9,14 @@ import { Product } from "./product";
 })
 export class ProductListComponent implements OnInit{
 
-    
+    constructor(private productService: ProductService){}
 
     imageWidth: number = 50;
     imageMargin: number = 2;
     pageTitle: string = 'Product List';
     showImages: boolean = false
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
 
     private _listFiler: string = '';
     get listFiler(): string{
@@ -32,68 +35,15 @@ export class ProductListComponent implements OnInit{
     onRatingClicked(message: string): void{
       this.pageTitle = 'Product List: ' + message;
     }
-
-    products: Product[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2021",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2021",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2021",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "assets/images/hammer.png"
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2021",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": "assets/images/saw.png"
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2020",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": "assets/images/xbox-controller.png"
-        }
-    ];
     
-    filteredProducts: Product[] = [];
-
     toggleImages(): void {
         this.showImages = !this.showImages;
     } 
 
     ngOnInit(): void{
         this.listFilter = '';
+        this.products = this.productService.getProducts()
+        this.filteredProducts = this.products
     }
 
 }
