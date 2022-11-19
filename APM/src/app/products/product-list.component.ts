8503e16d581744479b16c09f9c1608d00b3e5e4c
unknown <innocent.elorm.fiadu@gmail.com>
2022-11-19 14:38:27 +0000

    Defining Interfaces

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-product',
@@ -11,7 +12,7 @@ export class ProductListComponent{
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = '';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -32,6 +33,36 @@ export class ProductListComponent{
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
           },
+          {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2021",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "assets/images/hammer.png"
+          },
+          {
+            "productId": 8,
+            "productName": "Saw",
+            "productCode": "TBX-0022",
+            "releaseDate": "May 15, 2021",
+            "description": "15-inch steel blade hand saw",
+            "price": 11.55,
+            "starRating": 3.7,
+            "imageUrl": "assets/images/saw.png"
+          },
+          {
+            "productId": 10,
+            "productName": "Video Game Controller",
+            "productCode": "GMG-0042",
+            "releaseDate": "October 15, 2020",
+            "description": "Standard two-button video game controller",
+            "price": 35.95,
+            "starRating": 4.6,
+            "imageUrl": "assets/images/xbox-controller.png"
+          }
     ];
 
     toggleImage(): void{
