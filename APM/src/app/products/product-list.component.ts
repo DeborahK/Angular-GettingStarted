743eb6641cb273fe2c606d5e53a0debd81a89503
unknown <innocent.elorm.fiadu@gmail.com>
2022-11-19 12:06:53 +0000

    Handling events with Event Binding

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -9,6 +9,7 @@ export class ProductListComponent{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 1,
@@ -31,4 +32,8 @@ export class ProductListComponent{
             "imageUrl": "assets/images/garden_cart.png"
           },
     ];
+
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }
 }
