3c97e7b96c377d9062d72284fb2d3140830b6cb5
unknown <innocent.elorm.fiadu@gmail.com>
2022-11-19 11:52:51 +0000

    Added property binding to product component  referencing images

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -7,6 +7,8 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent{
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
             "productId": 1,
