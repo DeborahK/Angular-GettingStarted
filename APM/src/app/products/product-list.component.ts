a41e63f2c565de555132a9c2859cfda8c1ecfcb1
KiaZA91 <111753874+KiaZA91@users.noreply.github.com>
2023-07-11 15:43:14 +0200

    ProductArrayAdded

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -7,4 +7,26 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent {
  pageTitle: string = 'Product List';
+ products: any[] = [
+    {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      },
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2021",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png"
+      },
+ ]
 }
