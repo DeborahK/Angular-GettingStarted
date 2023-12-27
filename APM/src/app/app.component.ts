afdc3826a74131c29641cfbb406ad8b17cdb5010
benjdaun <benjdaun@pop-os.localdomain>
2023-12-27 16:22:51 -0500

    Basic products list loads

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
