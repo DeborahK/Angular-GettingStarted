64a6afba7e4ed97213d6a331755c2d4788221163
Victoria <veerod0804@yahoo.com>
2023-12-27 15:24:00 -0600

    added ConvertToSpaces pipe for product code

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -7,7 +7,7 @@ import { IProduct } from "./product";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -35,6 +35,10 @@ export class ProductListComponent {
     ];
     showImage: boolean = false;
     listFilter: string = 'cart';
+
+    ngOnInit(): void {
+        
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
