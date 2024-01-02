bc3bf98d4d5ad381edc4e18ee1f09921462169c3
Victoria <veerod0804@yahoo.com>
2024-01-02 14:34:42 -0600

    added ProductService for Product data.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -16,28 +17,7 @@ export class ProductListComponent implements OnInit{
         this.pageTitle = 
         'Product List: ' + message;
     };
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          }
-    ];
+    products: IProduct[] = [];
     showImage: boolean = false;
 
     private _listFilter: string = '';
@@ -53,7 +33,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart';
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
+    }
+
+    constructor(private productService: ProductService) {
+
     }
 
     toggleImage(): void {
---
+++ APM/src/app/products/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+
+    getProducts(): IProduct[] {
+        return [
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2021",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95,
+                "starRating": 3.2,
+                "imageUrl": "assets/images/leaf_rake.png"
+              },
+              {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2021",
+                "description": "15 gallon capacity rolling garden cart",
+                "price": 32.99,
+                "starRating": 4.2,
+                "imageUrl": "assets/images/garden_cart.png"
+              }
+        ]
+    }
+}
