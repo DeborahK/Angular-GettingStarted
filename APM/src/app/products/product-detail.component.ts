69c5dba47c65486e1e3f23a89b9ba8011723cd58
Victoria <veerod0804@yahoo.com>
2024-01-21 08:31:35 -0600

    added routerLink

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
